4841a6d499f5ddd643b3a6c130dc0849578135be
brayan2903 <71749066+brayan2903@users.noreply.github.com>
2023-08-22 22:22:18 -0500

    olv

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -35,9 +35,9 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-              <Link v-if="canResetPassword" :href="route('password.request')" class="text-sm text-gray-600 underline  hover:text-gray-900">
+              <!-- <Link v-if="canResetPassword" :href="route('password.request')" class="text-sm text-gray-600 underline  hover:text-gray-900">
                 Olvidaste tu contraseña?
-              </Link>
+              </Link> -->
 
               <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                 Iniciar Sesion
