7ff6e61f9b7f7d2d6b7f3ae69f9f7a5a9f32ccef
brayan2903 <71749066+brayan2903@users.noreply.github.com>
2023-09-07 16:29:38 -0500

    -

--- app/Http/Controllers/AvanceController.php
+++ app/Http/Controllers/AvanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Hash;
+use DB;
 
 class AvanceController extends Controller {
     
