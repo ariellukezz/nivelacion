f2e59c6ded1adab4eaa9177081a4a67f4c20bfa5
brayan2903 <71749066+brayan2903@users.noreply.github.com>
2023-09-09 20:23:28 -0500

    -

--- resources/js/Pages/Admin/Docente/index.vue
+++ resources/js/Pages/Admin/Docente/index.vue
@@ -17,7 +17,7 @@
         </div>
         <div class="card" >
         <Message :closable="false" style=" color: rgb(139, 62, 62)">Solo agregar Docente de su Escuela Profesional, en caso sea Docente de 
-        sevicio comunicarse con el Docente o Director de Escuela para su asignación.</Message>
+        servicio comunicarse con el Docente o Director de Escuela para su asignación.</Message>
     </div>
 
       </div>
