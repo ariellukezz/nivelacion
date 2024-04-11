1bef4fe36427289cf60828bc1d8958252886f9a0
brayan2903 <71749066+brayan2903@users.noreply.github.com>
2024-04-11 07:49:59 -0500

    visual para mobil estuduante
    visual para mobil estuduante

--- resources/js/Layouts/NavigationMobile.vue
+++ resources/js/Layouts/NavigationMobile.vue
@@ -17,12 +17,28 @@
       leave-to-class="opacity-0 transform -translate-x-20">
     <aside v-show="$page.props.showingMobileMenu" class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white md:hidden">
       <div class="py-4 text-gray-500 dark:text-gray-400">
-        <Link class="ml-6 text-lg font-bold text-gray-800" :href="route('dashboard')">
-        Windmill
+        
+
+
+      <Link class="text-lg font-bold text-blue-500" :href="route('dashboard')">
+        <div class="flex" style="align-items: center; margin-left: 20px; margin-top: 5px;">
+            <div style="margin-top: 6px;">
+              <img src="../../assets/imagenes/logotiny.png" width="30"/>
+            </div>
+            <div style="width: 1px; background-color: gray; margin: 0px 10px; height: 30px; margin-top: 8px;"></div>
+            <div style="margin-top: 12px;">
+              <span style="font-size: 1.2rem; font-weight: 700; letter-spacing: .300rem; font-family: Arial, Helvetica, sans-serif">VRA-UNA</span>
+                <div style="margin-top: -16px !important; "><span style="font-size: .63rem; letter-spacing: .38rem; font-weight: 500; font-family: Arial, Helvetica, sans-serif">NIVELACIÓN</span></div>
+                <!-- <div style=" margin-top: -12px; font-size: 0.9rem; letter-spacing: 0.03rem; font-weight: 500;">nivelación - académica</div> -->
+            </div>
+        </div>
       </Link>
+
+
         <ul class="mt-6">
+
           <li class="relative px-6 py-3">
-            <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+            <ResponsiveNavLink :href="route('estudiante-inicio')" :active="route().current('estudiante-inicio')">
               <template #icon>
                 <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                      stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -30,9 +46,44 @@
                       d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                 </svg>
               </template>
-              Dashboard
+              Inicio
             </ResponsiveNavLink>
           </li>
+
+
+          <li class="relative px-6 py-3">
+            <ResponsiveNavLink :href="route('estudiante-notas')" :active="route().current('estudiante-notas')">
+              <template #icon>
+                <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                     stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                  <path
+                      d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"></path>
+                </svg>
+              </template>
+              Cursos y Notas
+            </ResponsiveNavLink>
+          </li>
+
+
+          <li class="relative px-6 py-3">
+            <ResponsiveNavLink :href="route('estudiante-encuestas')" :active="route().current('estudiante-encuestas')">
+              <template #icon>
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                </svg>
+              </template>
+              Encuesta
+            </ResponsiveNavLink>
+          </li>
+
+
+
+
+
+
+<!-- 
 
           <li class="relative px-6 py-3">
             <ResponsiveNavLink :href="route('users.index')" :active="route().current('users.index')">
@@ -83,7 +134,7 @@
                 <a class="w-full" href="#">Child menu</a>
               </li>
             </ul>
-        </li>
+        </li> -->
         </ul>
       </div>
     </aside>
@@ -96,4 +147,11 @@ import { Link } from '@inertiajs/vue3';
 import { ref } from 'vue'
 
 let showingTwoLevelMenu = ref(false)
+
+
 </script>
+
+
+
+
+
